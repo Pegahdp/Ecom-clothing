f64190b096048d6b953915049ca7a170fb531123
Pegah Pasha <git config --global user.name pegahdp>
2022-11-14 12:42:51 +0800

    initial commit

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './directory.style.scss'
+import {CategoryItem} from '../category-item/category-item.component';
+
+export const Directory = ({categories}) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id}  category={category}/>
+      ))}
+    </div>
+  )
+}
